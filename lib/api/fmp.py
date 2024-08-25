2ae03d72e0c93bb4a97a46ce417c517de58b0bbb
faalbers <chalbers@gmail.com>
2024-08-25 10:21:10 -0700

    updated fmp and Polygon api

--- lib/api/fmp.py
+++ lib/api/fmp.py
@@ -3,18 +3,26 @@ import config, requests
 # Financial Modeling Prep API wrapper
 
 class FMP():
-    __urlMain = 'https://financialmodelingprep.com/api/'
-    
-    def __init__(self):
-        pass
-
-    def getStockList(self):
-        reqUrl = self.__urlMain+'v3/stock/list'
-        params = {'apikey': config.KEYS['FMP']['KEY']}
+    def __request(self, endpoint, params={}):
+        reqUrl = self.__urlMain+endpoint
+        params['apikey'] = config.KEYS['FMP']['KEY']
         result = requests.get(reqUrl, params)
         result = result.json()
         if 'Error Message' in result:
             print('FMP: ERROR: %s' % result['Error Message'])
-            return {}
+            return None
         return result
-    
+        
+    def __init__(self):
+        self.__urlMain = 'https://financialmodelingprep.com/api/'
+
+    def getStockList(self):
+        result = self.__request('v3/stock/list')
+        stockList = {}
+        if result == None:
+            return result
+        for item in result:
+            symbol = item['symbol']
+            item.pop('symbol')
+            stockList[symbol] = item
+        return stockList
--- lib/api/polygon.py
+++ lib/api/polygon.py
@@ -1,5 +1,46 @@
+import config, requests
+from time import sleep
+
 # Polygon API wrapper
 
 class Polygon():
+    def __request(self, endpoint, params={}):
+        nextUrl = self.__urlMain+endpoint
+        params['apikey'] = config.KEYS['POLYGON']['KEY']
+
+        reqCount = 0
+        plgData = []
+        while nextUrl != None:
+            if reqCount >= 5:
+                print('Polygon: 60 sec sleep after 5 request calls')
+                sleep(60)
+                reqCount = 0
+            result = requests.get(nextUrl, params=params)
+            reqCount += 1
+            if result.status_code == 200:
+                result = result.json()
+            else:
+                print('Polygon: Data request failed: %s' % nextUrl)
+                if result.status_code == 403:
+                    print('Error Message: %s' % result.json()['Error Message'])
+                    return None
+            plgData.append(result)
+            nextUrl = None
+            if 'next_url' in result:
+                nextUrl = result['next_url']
+        return plgData
+        
     def __init__(self):
-        pass
+        self.__urlMain = 'https://api.polygon.io/'
+
+    def getTickers(self):
+        result = self.__request('v3/reference/tickers', params={'limit': 1000})
+        if result == None:
+            return result
+        tickers = {}
+        for block in result:
+            for tickerData in block['results']:
+                ticker = tickerData.pop('ticker')
+                tickers[ticker] = tickerData
+
+        return tickers
