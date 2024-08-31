fa858c9572e472de58e845f182cd012e380f6d59
faalbers <chalbers@gmail.com>
2024-08-31 15:25:36 -0700

    added comments

--- lib/scrape/fmp.py
+++ lib/scrape/fmp.py
@@ -2,17 +2,22 @@ from lib import api
 from lib.tools import storage
 from datetime import datetime
 
+# retrieve data from Financial Modeling Prep API
+
 class FMP():
     def __init__(self):
         self.__dataPath = 'data/scrape/fmp'
     
     def get(self, refresh=False):
         if not refresh:
+            # return already scraped data
             return storage.get(self.__dataPath)
         
         data = {}
+        # retrieve data from API
         data['stocklist'] = api.FMP().getStocklist()
         
+        # macke a backup and save newly retrieved FMP data
         storage.backup(self.__dataPath)
         storage.save(self.__dataPath, data)
         
--- lib/scrape/saved.py
+++ lib/scrape/saved.py
@@ -3,7 +3,8 @@ import pandas as pd
 from pprint import pp
 
 class Saved():
-    def __getCSVB(self, name, keyName, fileName):
+    # CSV file loader into dict
+    def __getCSV(self, name, keyName, fileName):
         filePath = 'data/saved/'+fileName+'.csv'
         if os.path.exists(filePath):
             self.__data[name] = {}
@@ -18,27 +19,13 @@ class Saved():
             print('scrape.Saved: File does not exist: %s' % filePath)
             self.__data[name] = None
 
-    def __getCSV(self, name, keyName, fileName):
-        filePath = 'data/saved/'+fileName+'.csv'
-        if os.path.exists(filePath):
-            self.__data[name] = {}
-            self.__data[name]['timeStamp'] = int(os.path.getmtime(filePath))
-            data = pd.read_csv(filePath)
-            dataDict = data.T.to_dict()
-            self.__data[name]['data'] = {}
-            for index, data in dataDict.items():
-                key = data.pop(keyName)
-                if not key in self.__data[name]['data']:
-                    self.__data[name]['data'][key] = []
-                self.__data[name]['data'][key].append(data)
-        else:
-            print('scrape.Saved: File does not exist: %s' % filePath)
-            self.__data[name] = None
-
     def __init__(self):
         self.__data = {}
-        self.__getCSVB('MIC', 'MIC', 'ISO10383_MIC')
-        self.__getCSVB('country', 'Code', 'ISO3166-1')
+
+        # retrieve MIC exchanges data from save file
+        self.__getCSV('MIC', 'MIC', 'ISO10383_MIC')
+        # retrieve country ISO code
+        self.__getCSV('country', 'Code', 'ISO3166-1')
     
     def getData(self):
         return self.__data
