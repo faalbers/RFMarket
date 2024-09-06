efa21d2a1da73f055c346d0995a4e3bf73222dcc
faalbers <chalbers@gmail.com>
2024-09-06 15:00:25 -0700

    added yahoo data parse

--- rfmarket/scrape/__init__.py
+++ rfmarket/scrape/__init__.py
@@ -7,7 +7,6 @@ from copy import deepcopy
 from datetime import datetime
 from pprint import pp
 from ..tools import log
-
 __all__ = ['Scrape']
 
 class Scrape():
@@ -38,9 +37,16 @@ class Scrape():
             'nameBase': None,
             'symbolBase': None,
         },
+        'events': {
+            'dividends': None,
+            'splits': None,
+            'capitalGains': None,
+        },
         'timestamps': {
             'fmp': None,
             'polygon': None,
+            'yahooQuotes': None,
+            'yahooCharts': None,
             'price': None,
         },
     }
@@ -52,11 +58,12 @@ class Scrape():
                 refreshFMP=False,
                 refreshPolygon=False,
                 refreshYahoo=False):
+        
         log.info("Scrape Data from files and API's")
 
         # initialize user data for creation
         userData = {}
-
+        
         # get data from saved files under data/saved
         # read info.txt on how to retrieve them
         savedData = Saved().getData()
@@ -127,7 +134,7 @@ class Scrape():
             else:
                 userData[symbol]['timestamps']['polygon'] = tickerData['timestamp']            
             if 'name' in tickerData:
-                if userData[symbol]['name'] == None or len(tickerData['name']) > len(userData[symbol]['name']):
+                if userData[symbol]['name'] == None or (len(tickerData['name']) > len(userData[symbol]['name'])):
                     symbolsUpdated.add(symbol)
                     userData[symbol]['name'] = tickerData['name']
             if 'cik' in tickerData:
@@ -180,48 +187,79 @@ class Scrape():
         log.info('New symbols added to user data: %s' % len(symbolsNew))
         log.info('Symbols updated in user data  : %s' % len(symbolsUpdated))
 
-        # limit symbol search to US stock echanges
-        usSymbols = set()
+        # limit symbol user data to US stock exchanges
+        removeSymbols = set()
         for symbol, stockData in userData.items():
-            if stockData['exchange']['country'] == 'United States':
-                usSymbols.add(symbol)
-        usSymbols = list(usSymbols)
-        log.info('User data symbols with United States exchange count: %s' % len(usSymbols))
+            if stockData['exchange']['country'] != 'United States':
+                removeSymbols.add(symbol)
+        for symbol in removeSymbols: userData.pop(symbol)
+        log.info('User data symbols with United States exchange count: %s' % len(userData))
         
-        # # gather yahoo data
-        yahooData = Yahoo().get(usSymbols, refresh=refreshYahoo)
+        # gather yahoo data
+        symbols = list(userData.keys())
+        yahooData = Yahoo().get(symbols, refresh=refreshYahoo)
         log.info('Parse Yahoo data into user data')
-        # params = set()
-        # for symbol, quoteData in yahooData['quotes'].items():
-        #     modules = set((quoteData.keys()))
-        #     if 'quoteType' in quoteData:
-        #         modules.remove('quoteType')
-        #         moduleData = quoteData['quoteType']
-        #         if 'exchange' in moduleData and moduleData['exchange'] != None:
-        #             userData[symbol]['exchange']['cicode'] = moduleData['exchange']
-        #         if 'longName' in moduleData and moduleData['longName'] != None:
-        #             if len(moduleData['longName']) > len(userData[symbol]['name']):
-        #                 userData[symbol]['name'] = moduleData['longName']
-        #     if 'price' in quoteData:
-        #         modules.remove('price')
-        #         moduleData = quoteData['price']
-        #         # params = params.union(moduleData.keys())
-        #         if 'postMarketTime' in moduleData and moduleData['postMarketTime'] != None:
-        #             if userData[symbol]['price'] == None or userData[symbol]['timestamps']['price'] == None or\
-        #                 moduleData['postMarketTime'] > userData[symbol]['timestamps']['price']:
-        #                 userData[symbol]['price'] = moduleData['postMarketPrice']
-        #                 userData[symbol]['timestamps']['price'] = moduleData['postMarketTime']
-        #         if 'quoteType' in moduleData and moduleData['quoteType'] != None:
-        #             userData[symbol]['type']['assetType'] = moduleData['quoteType'].upper()
-        #         if 'currencySymbol' in moduleData and moduleData['currencySymbol'] != None:
-        #             userData[symbol]['currency']['symbolBase'] = moduleData['currencySymbol']
-        #         if 'currency' in moduleData and moduleData['currency'] != None:
-        #             userData[symbol]['currency']['symbol'] = moduleData['currency'].upper()
-        #     # pp(modules)
-        #     # break
-        # # pp(params)
+        params = set()
+        quoteModules = set()
+        quoteModulesDone = set()
+        for symbol, quoteData in yahooData['quotes'].items():
+            modules = set((quoteData.keys()))
+            userData[symbol]['timestamps']['yahooQuotes'] = quoteData['timestamp']
+            modules.remove('timestamp')
+            quoteModules = quoteModules.union(modules)
+            if 'quoteType' in quoteData:
+                quoteModulesDone.add('quoteType')
+                moduleData = quoteData['quoteType']
+                if 'exchange' in moduleData and moduleData['exchange'] != None:
+                    userData[symbol]['exchange']['cicode'] = moduleData['exchange']
+                if 'longName' in moduleData and moduleData['longName'] != None:
+                    if userData[symbol]['name'] == None or (len(moduleData['longName']) > len(userData[symbol]['name'])):
+                        userData[symbol]['name'] = moduleData['longName']
+            if 'price' in quoteData:
+                quoteModulesDone.add('price')
+                moduleData = quoteData['price']
+                priceTime = None
+                price = None
+                if 'postMarketPrice' in moduleData and moduleData['postMarketPrice'] != None:
+                    priceTime = moduleData['postMarketTime']
+                    price = moduleData['postMarketPrice']
+                elif 'regularMarketPrice' in moduleData and moduleData['regularMarketPrice'] != None:
+                    priceTime = moduleData['regularMarketTime']
+                    price = moduleData['regularMarketPrice']
+                if price != None:
+                    if userData[symbol]['timestamps']['price'] == None or (priceTime > userData[symbol]['timestamps']['price']):
+                        userData[symbol]['price'] = price
+                        userData[symbol]['timestamps']['price'] = priceTime
+                if 'quoteType' in moduleData and moduleData['quoteType'] != None:
+                    userData[symbol]['type']['assetType'] = moduleData['quoteType'].upper()
+                if 'currencySymbol' in moduleData and moduleData['currencySymbol'] != None:
+                    userData[symbol]['currency']['symbolBase'] = moduleData['currencySymbol']
+                if 'currency' in moduleData and moduleData['currency'] != None:
+                    userData[symbol]['currency']['symbol'] = moduleData['currency'].upper()
+        # pp(quoteModules.difference(quoteModulesDone))
+        chartTypes = set()
+        chartTypesDone = set()
+        for symbol, chartData in yahooData['charts'].items():
+            types = set((chartData.keys()))
+            userData[symbol]['timestamps']['yahoo'] = chartData['timestamp']
+            types.remove('timestamp')
+            chartTypes = chartTypes.union(types)
+            if 'events' in chartData:
+                chartTypesDone.add('events')
+                events = chartData['events']
+                # params = params.union(events.keys())
+                if 'dividends' in events:
+                    userData[symbol]['events']['dividends'] = events['dividends']
+                if 'splits' in events:
+                    userData[symbol]['events']['splits'] = events['splits']
+                if 'capitalGains' in events:
+                    userData[symbol]['events']['capitalGains'] = events['capitalGains']
+
+        # pp(chartTypes.difference(chartTypesDone))
+        # pp(params)
         
-        # pp(userData['AAPL'])
+        # pp(userData['ORKA'])
+        # pp(userData['MMM'])
 
         # return created user data
         log.info('Total user data symbols: %s' % len(userData))
