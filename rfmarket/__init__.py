8505085823e6c662f3053ea1263ebeb3aa645dfa
faalbers <chalbers@gmail.com>
2024-09-06 15:02:04 -0700

    last updates

--- rfmarket/__init__.py
+++ rfmarket/__init__.py
@@ -1,18 +1,38 @@
 from .scrape import *
-from .tools import log
+from .tools import log, storage
 
 class RFMarket():
+    mcalls = 1
+    mperiod = 1
+
     def __init__(self,
                 refreshFMP=False,
                 refreshPolygon=False,
                 refreshYahoo=False,
                 logLevel=log.WARNING):
+        
+        self.__dataPath = 'data/work/data'
         log.initLogger(logLevel=logLevel)
-        self.__data = Scrape().getData(
-                refreshFMP=refreshFMP,
-                refreshPolygon=refreshPolygon,
-                refreshYahoo=refreshYahoo)
+        if refreshFMP or refreshPolygon or refreshYahoo:
+            self.__data = Scrape().getData(
+                    refreshFMP=refreshFMP,
+                    refreshPolygon=refreshPolygon,
+                    refreshYahoo=refreshYahoo)
+            storage.backup(self.__dataPath)
+            storage.save(self.__dataPath, self.__data)
+
+        else:
+            self.__data = storage.get(self.__dataPath)
     
+    def getData(self):
+        return self.__data
+    
+    def getSymbolData(self, symbol):
+        symbol = symbol.upper()
+        if not symbol in self.__data: return None
+        return self.__data[symbol]
+
     @log.indent_decorator
     def logTest(self, a, b, c):
         log.info('BLAAHHH')
+    
--- rfmarket/scrape/yahoo.py
+++ rfmarket/scrape/yahoo.py
@@ -1,21 +1,37 @@
 from .. import api
-from ..tools import storage
+from ..tools import storage, log
 
 class Yahoo():
     def __init__(self):
         self.__dataPath = 'data/scrape/yahoo'
+        self.__dataPathFull = 'data/scrape/yahoo_full'
     
-    def get(self, symbols, refresh=False):
+    def get(self, symbols, refresh=False, fullData=False):
         if not refresh:
+            log.info('scrape.Yahoo retrieve data from storage')
+            if fullData:
+                return storage.get(self.__dataPathFull)
             return storage.get(self.__dataPath)
         
         data = {}
 
         yh = api.Yahoo()
-        # modules = api.Yahoo().getQuoteSummaryModules()
-        # data['quotes'] = api.Yahoo().getQuoteSummary(symbols, modules, verbose=True)
-        data['charts'] = yh.getCharts5y(symbols)
+        modules = api.Yahoo().getQuoteSummaryModules()
+        data['quotes'] = yh.getQuoteSummary(symbols, modules)
+
+        storage.backup(self.__dataPath)
+        storage.save(self.__dataPath, data)
+
+        data['charts'] = yh.getCharts(symbols, '5y', '1d')
         
+        # sve full data for if needed later
+        storage.save(self.__dataPathFull, data)
+        
+        # removing indicators, it's too much data
+        for symbol, symbolData in data['charts'].items():
+            if 'indicators' in symbolData:
+                symbolData.pop('indicators')
+
         storage.backup(self.__dataPath)
         storage.save(self.__dataPath, data)
         
